a4a30140450ad6bbd29e0ca186a47d1db43e4168
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2023-02-17 12:58:54 +0100

    script to show thead

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ $(document).ready(function () {
               url: "https://api.weatherapi.com/v1/forecast.json?key=f80af2edb12a45bc85d113841231302&q=" + city + "&days=7&aqi=no&alerts=no",
               type: "GET",
               dataType: "json",
-              success: function (data) {
-                  $("#city-name").html(data.location.name + ", " + data.location.country);
-                  $("#temp").html(data.current.temp_c + "&#8451;");
-                  $("#condition").html(data.current.condition.text);
-                  $("#icon").attr("src", "https:" + data.current.condition.icon);
-
+            success: function (data) {
+              $("#city-name").html(data.location.name + ", " + data.location.country);
+              $("#temp").html(data.current.temp_c + "&#8451;");
+              $("#condition").html(data.current.condition.text);
+              $("#icon").attr("src", "https:" + data.current.condition.icon);
+              
+              $("#weather-table").show();
                   // 7-day forecast
                   for (let i = 0; i < 7; i++) {
                       let forecast = data.forecast.forecastday[i];
