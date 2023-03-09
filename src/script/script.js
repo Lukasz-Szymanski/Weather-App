1c44cc5179876d2e79f1951f131b387cc3463daf
Łukasz Szymański <lukasz.szymanski94@icloud.com>
2023-03-09 12:20:22 +0100

    remove jquery

--- src/script/script.js
+++ src/script/script.js
@@ -1,38 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const weatherForm = document.querySelector(".weather-form");
+  const cityName = document.getElementById("city-name");
+  const temperature = document.getElementById("temp");
+  const condition = document.getElementById("condition");
+  const icon = document.getElementById("icon");
+  const weatherTable = document.getElementById("weather-table");
 
-$(document).ready(function () {
-  $(".weather-form").submit(function (event) {
-      event.preventDefault();
-      var city = $("#city").val();
+  weatherForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const city = document.getElementById("city").value;
 
-      if (city != "") {
-          $.ajax({
-              url: "https://api.weatherapi.com/v1/forecast.json?key=f80af2edb12a45bc85d113841231302&q=" + city + "&days=7&aqi=no&alerts=no",
-              type: "GET",
-              dataType: "json",
-            success: function (data) {
-              $("#city-name").html(data.location.name + ", " + data.location.country);
-              $("#temp").html(data.current.temp_c + "&#8451;");
-              $("#condition").html(data.current.condition.text);
-              $("#icon").attr("src", "https:" + data.current.condition.icon);
-              
-              $("#weather-table").show();
-                  // 7-day forecast
-                  for (let i = 0; i < 7; i++) {
-                      let forecast = data.forecast.forecastday[i];
-                      let date = forecast.date;
-                      let temp = forecast.day.avgtemp_c + "&#8451;";
-                      let condition = forecast.day.condition.text;
-                      let icon = "https:" + forecast.day.condition.icon;
+    if (city !== "") {
+      fetch(
+        "https://api.weatherapi.com/v1/forecast.json?key=f80af2edb12a45bc85d113841231302&q=" +
+          city +
+          "&days=7&aqi=no&alerts=no"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          cityName.innerHTML =
+            data.location.name + ", " + data.location.country;
+          temperature.innerHTML = data.current.temp_c + "&#8451;";
+          condition.innerHTML = data.current.condition.text;
+          icon.setAttribute("src", "https:" + data.current.condition.icon);
 
-                      $("#date-" + (i + 1)).html(date);
-                      $("#temp-" + (i + 1)).html(temp);
-                      $("#condition-" + (i + 1)).html(condition);
-                      $("#icon-" + (i + 1)).attr("src", icon);
-                  }
-              }
-          });
-      } else {
-          alert("City field cannot be empty");
-      }
+          weatherTable.style.display = "block";
+          // 7-day forecast
+          for (let i = 0; i < 7; i++) {
+            const forecast = data.forecast.forecastday[i];
+            if (forecast) {
+              const date = forecast.date;
+              const temp = forecast.day.avgtemp_c + "&#8451;";
+              const condition = forecast.day.condition.text;
+              const icon = "https:" + forecast.day.condition.icon;
+
+              document.getElementById("date-" + (i + 1)).innerHTML = date;
+              document.getElementById("temp-" + (i + 1)).innerHTML = temp;
+              document.getElementById("condition-" + (i + 1)).innerHTML =
+                condition;
+              document
+                .getElementById("icon-" + (i + 1))
+                .setAttribute("src", icon);
+            }
+          }
+        })
+        .catch((error) => console.error(error));
+    } else {
+      alert("City field cannot be empty");
+    }
   });
 });
